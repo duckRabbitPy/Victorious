b991113a38a66d0e8d84d7c93bb248a5c1522a2f
duckRabbitPy <78092825+duckRabbitPy@users.noreply.github.com>
2024-01-07 16:02:33 +0000

    update test user

--- server/src/test/websocket.test.ts
+++ server/src/test/websocket.test.ts
@@ -1,25 +1,21 @@
 import * as Effect from "@effect/io/Effect";
 import { beforeAll, describe, expect, it } from "vitest";
 import { DBConnectionTest, DBConnection } from "../db/connection";
-import { SEED_ROOM, resetAndSeedDatabase } from "../db/seed";
+import { TEST_ROOM, resetAndSeedDatabase, testUser1 } from "../db/seed";
 import { getLatestGameSnapshotQuery } from "../models/gamestate/queries";
 import {
   ClientPayload,
   SupportedEffects,
+  safeParseChatLog,
   safeParseGameState,
 } from "../../../shared/common";
 import { handleGameMessage } from "../websocketServer/handleGameMessage";
-
-const testUser = {
-  userId: "f47ac10b-58cc-4372-a567-0e02b2c3d479",
-  username: "testUser1",
-  authToken: "zI1NivsInR5tCI6IkpPVCJ9.eyJ1c2VySWQiOiIyYzLkZmIy",
-};
+import { handleChatMessage } from "../websocketServer/handleChatMessage";
 
 const getTestSession = Effect.provideService(
   DBConnection.pipe(
     Effect.flatMap((connection) => connection.pool),
-    Effect.flatMap((pool) => getLatestGameSnapshotQuery(SEED_ROOM, pool)),
+    Effect.flatMap((pool) => getLatestGameSnapshotQuery(TEST_ROOM, pool)),
     Effect.flatMap((gameState) => Effect.succeed(gameState)),
     Effect.flatMap(safeParseGameState)
   ),
@@ -37,16 +33,16 @@ describe("websocket message handling", () => {
 
     const room = initialGamestate.room;
     const testMsg = {
-      authToken: testUser.authToken,
+      authToken: testUser1.authToken,
       effect: SupportedEffects.addLivePlayer,
       room: room,
       cardName: undefined,
-      userId: testUser.userId,
+      userId: testUser1.userId,
       toDiscardFromHand: [],
       chatMessage: undefined,
     } as ClientPayload;
 
-    const processMessage = DBConnection.pipe(
+    const addNewPlayerToGame = DBConnection.pipe(
       Effect.flatMap((connection) => connection.pool),
       Effect.flatMap((pool) =>
         Effect.all({
@@ -59,21 +55,66 @@ describe("websocket message handling", () => {
           msg,
           pool,
           userInfo: {
-            userId: testUser.userId,
-            username: testUser.username,
+            userId: testUser1.userId,
+            username: testUser1.username,
           },
         })
       ),
       Effect.flatMap(safeParseGameState)
     );
     const runnable = Effect.provideService(
-      processMessage,
+      addNewPlayerToGame,
       DBConnection,
       DBConnectionTest
     );
 
     expect((await Effect.runPromise(runnable)).actor_state[0].id).toEqual(
-      testUser.userId
+      testUser1.userId
+    );
+  });
+
+  it("send chat message", async () => {
+    const initialGamestate = await Effect.runPromise(getTestSession);
+
+    const room = initialGamestate.room;
+    const testMsg = {
+      authToken: testUser1.authToken,
+      effect: SupportedEffects.sendChatMessage,
+      room: room,
+      cardName: undefined,
+      userId: testUser1.userId,
+      toDiscardFromHand: [],
+      chatMessage: "test chat message",
+    } as ClientPayload;
+
+    const sendChatMessage = DBConnection.pipe(
+      Effect.flatMap((connection) => connection.pool),
+      Effect.flatMap((pool) =>
+        Effect.all({
+          pool: Effect.succeed(pool),
+          msg: Effect.succeed(testMsg),
+        })
+      ),
+      Effect.flatMap(({ msg, pool }) =>
+        handleChatMessage({
+          msg,
+          pool,
+          userInfo: {
+            userId: testUser1.userId,
+            username: testUser1.username,
+          },
+        })
+      ),
+      Effect.flatMap(safeParseChatLog)
+    );
+    const runnable = Effect.provideService(
+      sendChatMessage,
+      DBConnection,
+      DBConnectionTest
+    );
+
+    expect((await Effect.runPromise(runnable))[0].message).toEqual(
+      testMsg.chatMessage
     );
   });
 });
